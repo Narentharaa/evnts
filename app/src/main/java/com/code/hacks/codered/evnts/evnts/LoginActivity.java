7c61cc84278b4312adccc043263636d344e89158
Narentharaa <naren180891@gmail.com>
2015-11-21 18:05:24 -0600

    Update

--- app/src/main/java/com/code/hacks/codered/evnts/evnts/LoginActivity.java
+++ app/src/main/java/com/code/hacks/codered/evnts/evnts/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.code.hacks.codered.evnts.evnts;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.code.hacks.codered.evnts.evnts.views.CustomButton;
+import com.code.hacks.codered.evnts.evnts.views.CustomEditText;
 
 /**
  * Created by sudharti on 11/21/15.
@@ -11,6 +15,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private CharSequence mTitle;
     private Toolbar mToolbar;
+    private CustomEditText CustomEditText;
+    private CustomEditText email;
+    private CustomEditText Password;
+    private CustomButton Login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        email=(CustomEditText)findViewById(R.id.email);
+        // function to validate the email
+
+        Password=(CustomEditText)findViewById(R.id)
+
+
 
     }
+
 }
