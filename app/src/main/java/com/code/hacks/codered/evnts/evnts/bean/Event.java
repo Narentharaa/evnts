23a96e2d354d765eaa8978b739691a7e166517af
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-11-21 19:42:22 -0600

    Added Event List Adapter

---
+++ app/src/main/java/com/code/hacks/codered/evnts/evnts/bean/Event.java
@@ -0,0 +1,24 @@
+package com.code.hacks.codered.evnts.evnts.bean;
+
+/**
+ * Created by sudharti on 11/21/15.
+ */
+public class Event {
+    private String name, imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- app/src/main/java/com/code/hacks/codered/evnts/evnts/fragments/HomeFragment.java
+++ app/src/main/java/com/code/hacks/codered/evnts/evnts/fragments/HomeFragment.java
@@ -1,18 +1,26 @@
 package com.code.hacks.codered.evnts.evnts.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.code.hacks.codered.evnts.evnts.R;
+import com.code.hacks.codered.evnts.evnts.adapters.EventListAdapter;
 
 /**
  * Created by sudharsanan on 4/15/15.
  */
 public class HomeFragment extends Fragment {
+
+    private RecyclerView eventRecyclerView;
+    private RecyclerView.LayoutManager recylerLayoutManager;
+    private EventListAdapter recylerAdapter;
 
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -22,6 +30,27 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        eventRecyclerView = (RecyclerView) rootView.findViewById(R.id.event_recycler_view);
+        eventRecyclerView.setHasFixedSize(true);
+
+        recylerLayoutManager = new LinearLayoutManager(getActivity());
+        eventRecyclerView.setLayoutManager(recylerLayoutManager);
+
         return rootView;
+    }
+
+    private class FetchEvents extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+        }
     }
 }
