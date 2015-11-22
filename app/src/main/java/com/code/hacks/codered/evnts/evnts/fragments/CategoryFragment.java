50982f70ef094664224619e36c76be73f6e0a492
Sandeep Ganesan <sandeep.ganesan@gmail.com>
2015-11-22 08:48:10 -0600

    Category got from REST call

--- app/src/main/java/com/code/hacks/codered/evnts/evnts/fragments/CategoryFragment.java
+++ app/src/main/java/com/code/hacks/codered/evnts/evnts/fragments/CategoryFragment.java
@@ -1,6 +1,6 @@
 package com.code.hacks.codered.evnts.evnts.fragments;
 
-import android.os.AsyncTask;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
@@ -8,11 +8,21 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.code.hacks.codered.evnts.evnts.R;
 import com.code.hacks.codered.evnts.evnts.adapters.NavDrawerAdapter;
 import com.code.hacks.codered.evnts.evnts.bean.DrawerItem;
+import com.code.hacks.codered.evnts.evnts.models.Categories;
+import com.code.hacks.codered.evnts.evnts.models.Category;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by sandeep on 11/21/2015.
@@ -21,6 +31,7 @@ public class CategoryFragment extends ListFragment {
     boolean mDualPane;
     int mCurCheckPosition = 0;
     private ArrayList<DrawerItem> categoryList;
+    Context context;
 
     public static CategoryFragment newInstance() {
         CategoryFragment fragment = new CategoryFragment();
@@ -30,15 +41,18 @@ public class CategoryFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        DrawerItem item = (DrawerItem)l.getAdapter().getItem(position);
+        DrawerItem item = (DrawerItem) l.getAdapter().getItem(position);
         Toast.makeText(v.getContext(), item.getAction() + item.getId(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        categoryList = new ArrayList<DrawerItem>();
-        new FetchCategories().execute();
+
+        context = getContext();
+        addCategories();
+//        categoryList = new ArrayList<DrawerItem>();
+//        new FetchCategories().execute();
     }
 
     @Override
@@ -47,33 +61,80 @@ public class CategoryFragment extends ListFragment {
         outState.putInt("curChoice", mCurCheckPosition);
     }
 
-    private class FetchCategories extends AsyncTask<Void, Integer, Void> {
+//    private class FetchCategories extends AsyncTask<Void, Integer, Void> {
+//
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            categoryList = addCategories();
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void aVoid) {
+//            super.onPostExecute(aVoid);
+//            setListAdapter(new NavDrawerAdapter(getContext(), categoryList));
+//        }
+//    }
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            categoryList = addCategories();
-            return null;
-        }
+    HashMap<String, Integer> iconMap;
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            setListAdapter(new NavDrawerAdapter(getContext(), categoryList));
-        }
+    private void createIconMap() {
+        iconMap = new HashMap<>();
+        iconMap.put("Sports", R.mipmap.ic_action_football_100);
+        iconMap.put("Cultural", R.mipmap.ic_action_dancing_100);
+        iconMap.put("Volunteering", R.mipmap.ic_action_helping_hand_100);
+        iconMap.put("Studies", R.mipmap.ic_action_books_100);
+        iconMap.put("Fun", R.mipmap.ic_action_wink_100);
+        iconMap.put("Debate/Talk", R.mipmap.ic_action_strike_100);
+        iconMap.put("Clubs", R.mipmap.ic_action_clubs_100);
+        iconMap.put("Life Skills", R.mipmap.ic_action_life_cycle_100);
+        iconMap.put("Food", R.mipmap.ic_action_food_filled_100);
+        iconMap.put("Jobs", R.mipmap.ic_action_suitcase_100);
+
     }
 
-    private ArrayList<DrawerItem> addCategories() {
-        categoryList.add(new DrawerItem("Studies", R.mipmap.ic_action_books_100, 1));
-        categoryList.add(new DrawerItem("Cultural", R.mipmap.ic_action_dancing_100, 2));
-        categoryList.add(new DrawerItem("Volunteering", R.mipmap.ic_action_helping_hand_100, 3));
-        categoryList.add(new DrawerItem("Fun", R.mipmap.ic_action_wink_100, 4));
-        categoryList.add(new DrawerItem("Clubs", R.mipmap.ic_action_clubs_100, 5));
-        categoryList.add(new DrawerItem("Life Skills", R.mipmap.ic_action_life_cycle_100, 6));
-        categoryList.add(new DrawerItem("Food", R.mipmap.ic_action_food_filled_100, 7));
-        categoryList.add(new DrawerItem("Jobs", R.mipmap.ic_action_suitcase_100, 8));
-        categoryList.add(new DrawerItem("Debates", R.mipmap.ic_action_strike_100, 9));
+    private int getIcon(String key) {
+        return iconMap != null ? iconMap.get(key) : -1;
+    }
 
-        return categoryList;
+    private void addCategories() {
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        createIconMap();
+
+        StringRequest sr = new StringRequest(Request.Method.GET, "http://6172ea19.ngrok.io/api/v1/categories",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new Gson();
+                        Categories categories = gson.fromJson(response, Categories.class);
+                        ArrayList<DrawerItem> categoryList = new ArrayList<>();
+                        for (Category category : categories.getCategories())
+                            categoryList.add(new DrawerItem(category.getName(), iconMap.get(category.getName())));
+                        setListAdapter(new NavDrawerAdapter(context, categoryList));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Error while fetching categories.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(sr);
+
+
+//        categoryList.add(new DrawerItem("Studies", R.mipmap.ic_action_books_100, 1));
+//        categoryList.add(new DrawerItem("Cultural", R.mipmap.ic_action_dancing_100, 2));
+//        categoryList.add(new DrawerItem("Volunteering", R.mipmap.ic_action_helping_hand_100, 3));
+//        categoryList.add(new DrawerItem("Fun", R.mipmap.ic_action_wink_100, 4));
+//        categoryList.add(new DrawerItem("Clubs", R.mipmap.ic_action_clubs_100, 5));
+//        categoryList.add(new DrawerItem("Life Skills", R.mipmap.ic_action_life_cycle_100, 6));
+//        categoryList.add(new DrawerItem("Food", R.mipmap.ic_action_food_filled_100, 7));
+//        categoryList.add(new DrawerItem("Jobs", R.mipmap.ic_action_suitcase_100, 8));
+//        categoryList.add(new DrawerItem("Debates", R.mipmap.ic_action_strike_100, 9));
+//
+//        return categoryList;
     }
 
 }
